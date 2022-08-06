bf83dabbc1a909381626fb26b5750ca19d1ab5ca
ereshidov <edem@neattech.io>
2022-08-06 20:35:45 +0300

    add: more expressJs setup

--- server/index.js
+++ server/index.js
@@ -4,12 +4,49 @@ const cors = require("cors");
 
 const { Pool } = require("pg");
 
+const redis = require("redis");
+
 const config = require("./config");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
+
+  res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hGetAll("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+
+  redisClient.hSet("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log("Listening on port 5000...");
+});
 
 const pgClient = new Pool({
   user: config.pgUser,
@@ -26,3 +63,11 @@ pgClient
   .catch((err) => {
     console.log(err);
   });
+
+const redisClient = redis.createClient({
+  host: config.redisHost,
+  port: config.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
